1ae2970755df47a4faeb8e014243b57e90b5d98f
Arthur Bisharyan <bisharyanav@gmail.com>
2019-11-05 18:04:40 +0300

    fixed bugs

--- quiz/client/src/app.js
+++ quiz/client/src/app.js
@@ -2,8 +2,12 @@ function getRandomIntFromRange(min, max) {
     return Math.floor(min + Math.random() * (max - min + 1));
 }
 
+function deepClone(object) {
+    return JSON.parse(JSON.stringify(object));
+}
+
 function shuffleArray(array) {
-    var shuffledArray = array;
+    var shuffledArray = deepClone(array);
     for (var currentId = array.length - 1; currentId > 0; currentId--) {
         var otherId = getRandomIntFromRange(0, currentId);
         [shuffledArray[currentId], shuffledArray[otherId]] = [shuffledArray[otherId], shuffledArray[currentId]];
@@ -11,17 +15,6 @@ function shuffleArray(array) {
 
     return shuffledArray;
 }
-
-var db = [{"text":"aliquet pulvinar sed nisl nunc rhoncus dui vel sem sed sagittis nam congue risus semper porta volutpat quam pede lobortis","answer":"adapter"},
-{"text":"viverra dapibus nulla suscipit ligula in lacus curabitur at ipsum ac tellus","answer":"multimedia"},
-{"text":"vestibulum quam sapien varius ut blandit non interdum in ante vestibulum ante ipsum primis","answer":"time-frame"},
-{"text":"dapibus augue vel accumsan tellus nisi eu orci mauris lacinia sapien quis libero nullam","answer":"solution"},
-{"text":"quis libero nullam sit amet turpis elementum ligula vehicula consequat morbi a ipsum integer a nibh in quis justo maecenas","answer":"framework"},
-{"text":"amet cursus id turpis integer aliquet massa id lobortis convallis","answer":"complexity"},
-{"text":"hac habitasse platea dictumst aliquam augue quam sollicitudin vitae consectetuer eget rutrum at lorem integer tincidunt ante vel ipsum praesent","answer":"Operative"},
-{"text":"mauris enim leo rhoncus sed vestibulum sit amet cursus id turpis integer aliquet massa id lobortis","answer":"global"},
-{"text":"risus auctor sed tristique in tempus sit amet sem fusce consequat nulla","answer":"focus group"},
-{"text":"in faucibus orci luctus et ultrices posuere cubilia curae mauris","answer":"context-sensitive"}];
 
 function hasNextQuestion(questions) {
     return questions.length > 0;
@@ -32,7 +25,7 @@ function getNextQuestion(questions) {
 }
 
 function askQuestion(question) {  
-    var answer = prompt(nextQuestion.text);
+    var answer = prompt(question.text);
     return answer;
 }
 
@@ -49,12 +42,23 @@ function runQuiz(questions) {
     var answeredCorrect = 0;
 
     while (hasNextQuestion(shuffledQuestions) && isTimerEnabled()) {
-        var nextQuestion = getNextQuestion(shuffledQuestions);
-        var answer = askQuestion(nextQuestion);        
-        if (isAnswerCorrect(nextQuestion, answer)) {
+        var question = getNextQuestion(shuffledQuestions);
+        var answer = askQuestion(question);        
+        if (isAnswerCorrect(question, answer)) {
             answeredCorrect++;
         }
     }
 
-    console.info('Correct: ' + answeredCorrect + ' out of ' + questions.length);
+    console.info('Correct: ' + answeredCorrect + ' out of ' + db.length);
 }
+
+var db = [{"text":"aliquet pulvinar sed nisl nunc rhoncus dui vel sem sed sagittis nam congue risus semper porta volutpat quam pede lobortis","answer":"adapter"},
+{"text":"viverra dapibus nulla suscipit ligula in lacus curabitur at ipsum ac tellus","answer":"multimedia"},
+{"text":"vestibulum quam sapien varius ut blandit non interdum in ante vestibulum ante ipsum primis","answer":"time-frame"},
+{"text":"dapibus augue vel accumsan tellus nisi eu orci mauris lacinia sapien quis libero nullam","answer":"solution"},
+{"text":"quis libero nullam sit amet turpis elementum ligula vehicula consequat morbi a ipsum integer a nibh in quis justo maecenas","answer":"framework"},
+{"text":"amet cursus id turpis integer aliquet massa id lobortis convallis","answer":"complexity"},
+{"text":"hac habitasse platea dictumst aliquam augue quam sollicitudin vitae consectetuer eget rutrum at lorem integer tincidunt ante vel ipsum praesent","answer":"Operative"},
+{"text":"mauris enim leo rhoncus sed vestibulum sit amet cursus id turpis integer aliquet massa id lobortis","answer":"global"},
+{"text":"risus auctor sed tristique in tempus sit amet sem fusce consequat nulla","answer":"focus group"},
+{"text":"in faucibus orci luctus et ultrices posuere cubilia curae mauris","answer":"context-sensitive"}];
